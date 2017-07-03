be470b909030b83d01a411bd7831e2a36a914ad3
cybras <aaron_puhl@web.de>
2017-07-03 16:15:09 +0200

    Initial Source Code commit
    * Http2PushModule solution

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HTTP2PushModule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HTTP2PushModule")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3a83e288-dad4-467d-ad50-68eb2377cd17")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/PushGroupSection.cs
@@ -0,0 +1,94 @@
+﻿using System.Configuration;
+
+namespace ZDV.HTTP2PushModule.Configuration
+{
+    /// <summary>
+    /// The actual PushGroup - has a name to be user friendly and a list of PushElements
+    /// </summary>
+    public class PushGroupElement : ConfigurationElement
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        public PushElementCollection PushElements => (PushElementCollection)base[""];
+
+        /// <summary>
+        /// Name to be human-readable
+        /// </summary>
+        [ConfigurationProperty("name", IsRequired = true, IsKey = true, DefaultValue = "")]
+        public string Name
+        {
+            get => (string)this["name"];
+            set => this["name"] = value;
+        }
+    }
+
+    
+    /// <summary>
+    /// A PushElement is part of a PushGroup and can be triggering or not.
+    /// A triggering PushElement, will create push promises for all other elements of the PushGroup.
+    /// </summary>
+    public class PushElement : ConfigurationElement
+    {
+        /// <summary>
+        /// Application Relative path to the file, that should be part of the push promise.
+        /// </summary>
+        [ConfigurationProperty("url", IsRequired = true, IsKey = true, DefaultValue = "")]
+        public string Url
+        {
+            get => (string)this["url"];
+            set => this["url"] = value;
+        }
+
+        /// <summary>
+        /// True if a request to the file should trigger the push-promise group
+        /// </summary>
+        [ConfigurationProperty("triggers", IsKey = true, DefaultValue = false)]
+        public bool Triggers
+        {
+            get => (bool)this["triggers"];
+            set => this["triggers"] = value;
+        }
+    }
+
+    #region Configuration Infrastructure
+    /// <summary>
+    /// Root element of PushGroup Configuration.
+    /// </summary>
+    class PushGroupSection : ConfigurationSection
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        public PushGroupCollection PushGroup => (PushGroupCollection)this[""];
+    }
+
+    /// <summary>
+    /// Collection of PushGroups. Needed by configuration infrastructure
+    /// </summary>
+    public class PushGroupCollection : ConfigurationElementCollection
+    {
+        public PushGroupElement this[object key] => BaseGet(key) as PushGroupElement;
+
+        public override ConfigurationElementCollectionType CollectionType => ConfigurationElementCollectionType.BasicMap;
+
+        protected override string ElementName => "PushGroup";
+
+        protected override ConfigurationElement CreateNewElement() => new PushGroupElement();
+
+        protected override object GetElementKey(ConfigurationElement element) => (PushGroupElement)element;
+    }
+
+    /// <summary>
+    /// PushElement Collection, needed by configuration infrastructure
+    /// </summary>
+    public class PushElementCollection : ConfigurationElementCollection
+    {
+        public PushElement this[object key] => BaseGet(key) as PushElement;
+
+        public override ConfigurationElementCollectionType CollectionType => ConfigurationElementCollectionType.BasicMap;
+
+        protected override string ElementName => "PushElement";
+
+        protected override ConfigurationElement CreateNewElement() => new PushElement();
+
+        protected override object GetElementKey(ConfigurationElement element) => (PushElement)element;
+    }
+    #endregion
+}
